48bd427ab7af47ea0ee7a9a6a8378a5e07d52c3e
Damir <kdamir2004@gmail.com>
2024-11-18 21:22:27 +0500

    init project2

---
+++ project2/StudentManagementSystem/students/models.py
@@ -0,0 +1,7 @@
+# from django.db import models
+
+# class Student(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     dob = models.DateField()
+#     registration_date = models.DateField(auto_now_add=True)
+
---
+++ project2/StudentManagementSystem/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ROLE_CHOICES = (
+        ('Student', 'Student'),
+        ('Teacher', 'Teacher'),
+        ('Admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
